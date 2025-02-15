5e3c33c0128166d4606b13f7b5b6265bfaee1810
Raman Turyk <roman.turik48@gmail.com>
2025-02-15 18:36:57 +0300

    task1

---
+++ script.py
@@ -0,0 +1,4 @@
+# Are you there? We’ve opened up a communications link to The Fender‘s secret computer. We need you to write a program that will read in the compromised usernames and passwords that are stored in a file called "passwords.csv".
+#
+# First import the CSV module, since we’ll be needing it to parse the data.
+import csv
